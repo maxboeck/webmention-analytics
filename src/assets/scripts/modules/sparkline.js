37e556ebc596c552838cd2306163924bc202c0bc
Max Böck <hello@mxb.at>
2021-02-10 22:21:10 +0100

    add sparklines

---
+++ src/assets/scripts/modules/sparkline.js
@@ -0,0 +1,64 @@
+/* Canvas-Sparkline by Jeremy Keith */
+/* https://github.com/adactio/Canvas-Sparkline */
+
+const SELECTORS = {
+    sparkline: '.js-sparkline'
+}
+
+function drawSparkline(canvasElement, data, endpoint, color, style) {
+    if (window.HTMLCanvasElement) {
+        var c = canvasElement,
+            ctx = c.getContext('2d'),
+            color = color ? color : 'rgba(0,0,0,0.5)',
+            style = style == 'bar' ? 'bar' : 'line',
+            height = c.height - 6,
+            width = c.width,
+            total = data.length,
+            max = Math.max.apply(Math, data),
+            xstep = width / total,
+            ystep = max / height,
+            x = 0,
+            y = height - data[0] / ystep,
+            i
+        if (window.devicePixelRatio) {
+            c.width = c.width * window.devicePixelRatio
+            c.height = c.height * window.devicePixelRatio
+            c.style.width = c.width / window.devicePixelRatio + 'px'
+            c.style.height = c.height / window.devicePixelRatio + 'px'
+            c.style.display = 'inline-block'
+            ctx.scale(window.devicePixelRatio, window.devicePixelRatio)
+        }
+        ctx.clearRect(0, 0, width, height)
+        ctx.beginPath()
+        ctx.strokeStyle = color
+        ctx.moveTo(x, y)
+        for (i = 1; i < total; i = i + 1) {
+            x = x + xstep
+            y = height - data[i] / ystep + 2
+            if (style == 'bar') {
+                ctx.moveTo(x, height)
+            }
+            ctx.lineTo(x, y)
+        }
+        ctx.stroke()
+        if (endpoint && style == 'line') {
+            ctx.beginPath()
+            ctx.fillStyle = '#00bff8'
+            ctx.arc(x, y, 3, 0, Math.PI * 2)
+            ctx.fill()
+        }
+    }
+}
+
+const containers = document.querySelectorAll(SELECTORS.sparkline)
+if (containers) {
+    containers.forEach((container) => {
+        const canvas = container.querySelector('canvas')
+        const values = container.dataset.values
+
+        if (canvas && values) {
+            const data = values.split(',').map((i) => parseInt(i, 10))
+            drawSparkline(canvas, data, true, '#FFF')
+        }
+    })
+}
--- utils/shortcodes.js
+++ utils/shortcodes.js
@@ -1,3 +1,5 @@
+const { DateTime } = require('luxon')
+
 module.exports = {
     icon: function (name) {
         return `<svg class="icon icon--${name}" role="img" aria-hidden="true" width="24" height="24">
@@ -9,5 +11,33 @@ module.exports = {
         const json = JSON.stringify(data)
         return `<div class="chartcontainer js-chart-container" data-source="chart-data-${id}"></div>
                 <script type="application/json" id="chart-data-${id}">${json}</script>`
+    },
+
+    sparkline: function (report) {
+        const now = DateTime.utc()
+        const endDate = DateTime.fromISO(report.to, { zone: 'utc' })
+        let daysOffset = 0
+
+        if (endDate > now) {
+            const diff = endDate.diff(now, ['days']).toObject()
+            daysOffset = Math.ceil(diff.days)
+        }
+
+        const series = report.data.timeseries.series.map((s) => s.data)
+        const range = series[0].length - daysOffset
+        const totals = []
+
+        for (let i = 0; i < range; i++) {
+            let sum = 0
+            for (let j = 0; j < series.length; j++) {
+                const value = series[j][i]
+                if (typeof value === 'number') {
+                    sum += value
+                }
+            }
+            totals.push(sum)
+        }
+        const csv = totals.join(',')
+        return `<div class="sparkline js-sparkline" data-values="${csv}"><canvas width="150" height="30"></canvas></div>`
     }
 }
